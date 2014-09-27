72b4c5f239105c5ff16fbe6e1ae026e51bb58306
matthew bullington <ww2freak999@yahoo.com>
2014-09-26 19:55:03 -0700

    added game icon, added resource folder

--- MyGame/MyGame/Form1.Designer.vb
+++ MyGame/MyGame/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.SuspendLayout()
         '
         'Form1
@@ -29,6 +30,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(728, 494)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
